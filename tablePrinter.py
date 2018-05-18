7816fceb796806f642da30bb2f7e8cc171d30da1
ektorasgr <38502735+ektorasgr@users.noreply.github.com>
2018-05-17 19:56:44 -0700

    Add files via upload

--- tablePrinter.py
+++ tablePrinter.py
@@ -9,5 +9,28 @@ tableData = [['apples', 'oranges', 'cherries', 'banana'],
              ['Alice', 'Bob', 'Carol', 'David'],
              ['dogs', 'cats', 'moose', 'goose']]
 
-def printTable():
-    colWidths = [0] * len(tableData)
+def printTable(Data):
+    #find the longest string of each list and store in colWidths
+    colWidths = [0] * len(Data)
+    for i in range(len(Data)):
+        for k in range(len(Data[i])):
+            if len(Data[i][k]) > colWidths[i]:
+                colWidths[i] = len(Data[i][k])
+            #else:
+                #continue
+    #find the longest string of the sublists and store in longest
+    longest = 0
+    for i in range(len(colWidths)):
+        if colWidths[i] > longest:
+            longest = colWidths[i]
+        #else:
+            #continue
+    for i in range(len(Data[i])):
+        for k in range(len(Data)):
+            if k < (len(Data)-1):
+                print(Data[k][i].rjust(longest), end = '')
+            else:
+                print(Data[k][i].rjust(longest))
+
+
+printTable(tableData)
